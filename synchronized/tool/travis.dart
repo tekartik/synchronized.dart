f409affc6b22ab20715d8d51a4baeedcddd044a0
Simon Binder <oss@simonbinder.eu>
2020-11-18 22:26:44 +0100

    Use Dart 2.12 for null safety

--- synchronized/tool/travis.dart
+++ synchronized/tool/travis.dart
@@ -1,28 +1,27 @@
+//@dart=2.9
+import 'dart:io';
+
 import 'package:process_run/shell.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 Future<void> main() async {
   var shell = Shell();
 
-  var nnbdEnabled = dartVersion > Version(2, 10, 0, pre: '92');
-  var dartExtraOptions = '';
+  var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
   var dartRunExtraOptions = '';
+  var dart = Platform.executable;
+
   if (nnbdEnabled) {
-    // Temp dart extra option. To remove once nnbd supported on stable without flags
-    dartExtraOptions = '--enable-experiment=non-nullable';
     // Needed for run and test
-    dartRunExtraOptions =
-        '--enable-experiment=non-nullable --no-sound-null-safety';
+    dartRunExtraOptions = '--no-sound-null-safety';
 
     await shell.run('''
 
-dartanalyzer $dartExtraOptions --fatal-warnings --fatal-infos .
-dartfmt -n --set-exit-if-changed .
+$dart analyze --fatal-warnings --fatal-infos .
+$dart format -o none --set-exit-if-changed .
 
-pub run $dartRunExtraOptions test -p vm -j 1
-# NNBD failing - dart $dartRunExtraOptions pub run build_runner test -- -p vm -j 1
-# NNBD failing - pub run $dartRunExtraOptions test -p chrome,firefox -j 1
-# NNBD failing - pub run $dartRunExtraOptions build_runner test -- -p chrome -j 1
+$dart $dartRunExtraOptions run  test -p vm,chrome,firefox -j 1
+$dart $dartRunExtraOptions run build_runner test -- -p vm,chrome -j 1
   ''');
   }
 }
