fa89d9ff3073b45b3d6fa9e3bcf825ed832ea23b
alex <alex@tekartik.com>
2020-11-18 22:53:08 +0100

    test! remove all experimental flags from scripts

--- synchronized/tool/travis.dart
+++ synchronized/tool/travis.dart
@@ -1,5 +1,4 @@
 //@dart=2.9
-import 'dart:io';
 
 import 'package:process_run/shell.dart';
 import 'package:pub_semver/pub_semver.dart';
@@ -8,20 +7,14 @@ Future<void> main() async {
   var shell = Shell();
 
   var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
-  var dartRunExtraOptions = '';
-  var dart = Platform.executable;
-
   if (nnbdEnabled) {
-    // Needed for run and test
-    dartRunExtraOptions = '--no-sound-null-safety';
-
     await shell.run('''
 
-$dart analyze --fatal-warnings --fatal-infos .
-$dart format -o none --set-exit-if-changed .
+dart analyze --fatal-warnings --fatal-infos .
+dart format -o none --set-exit-if-changed .
 
-$dart $dartRunExtraOptions run  test -p vm,chrome,firefox -j 1
-$dart $dartRunExtraOptions run build_runner test -- -p vm,chrome -j 1
+dart test -p vm,chrome,firefox -j 1
+dart run build_runner test -- -p vm,chrome -j 1
   ''');
   }
 }
--- tool/travis.dart
+++ tool/travis.dart
@@ -7,21 +7,14 @@ Future<void> main() async {
   var shell = Shell();
 
   var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
-  var dartExtraOptions = '';
-  var dartRunExtraOptions = '';
-  var dart = Platform.executable;
-
   if (nnbdEnabled) {
-    // Needed for run and test
-    dartRunExtraOptions = '$dartExtraOptions --no-sound-null-safety';
-
     for (var dir in ['synchronized']) {
       shell = shell.pushd(dir);
       stdout.writeln('package: $dir');
       await shell.run('''
 
-$dart $dartExtraOptions pub get
-$dart $dartRunExtraOptions run tool/travis.dart
+dart pub get
+dart tool/travis.dart
 
     ''');
       shell = shell.popd();
